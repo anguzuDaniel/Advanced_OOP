e1d0c54ac12aed590671dcfdd28a24909d32f54c
anguzuDaniel <102733205+anguzuDaniel@users.noreply.github.com>
2022-09-13 10:54:23 +0300

    second commit
    Added line 7, 8 and 9

--- src/home/Main.java
+++ src/home/Main.java
@@ -2,6 +2,9 @@ package home;
 
 public class Main {
     public static void main(String[] args) {
+        int A = 10;
+        System.out.println("Welcome to git.");
+        System.out.println(A);
 
     }
 }
