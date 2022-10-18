74f4e7ab7b7e9dd66a655f332452e5adaca44d9a
anguzuDaniel <102733205+anguzuDaniel@users.noreply.github.com>
2022-10-18 11:51:01 +0300

    added sub panel to the layout

--- src/com/layout_objects/Menus.java
+++ src/com/layout_objects/Menus.java
@@ -2,10 +2,16 @@ package com.layout_objects;
 
 import javax.swing.*;
 
-public class Menus {
+public class Menus extends JFrame {
 
     JMenuBar menuBar;
-    JMenu menu;
+    JMenu menu, subMenu;
+
+    JPanel mainPanel;
+
+    JMenuItem itemOne;
+    JMenuItem itemTwo;
+    JMenuItem itemThree;
 
     public Menus() {
         this.prepareLayout();
@@ -16,19 +22,46 @@ public class Menus {
         JFrame menuFrame = new JFrame("Navigation menu");
         menuFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
-        menuFrame.add(this.prepare_JMenuBar());
-
-        menuFrame.setSize(500, 500);
+        menuFrame.setJMenuBar(this.prepare_JMenuBar());
+        menuFrame.add(this.prepare_JPanel());
         menuFrame.setVisible(true);
     }
 
     public final JMenuBar prepare_JMenuBar() {
         menuBar = new JMenuBar();
+        menuBar.add(this.prepare_JMenu());
         return menuBar;
     }
 
     public final JMenu prepare_JMenu() {
-        menu = new JMenu();
+        menu = new JMenu("Files");
+
+
+        menu.add(this.prepare_JMenuItem("New"));
+        menu.add(this.prepare_JMenuItem("Open"));
+        menu.add(this.prepare_JMenuItem("Open Recent"));
+
+        menu.add(this.prepare_SubJMenuItem("Export"));
+
         return menu;
+    }
+
+    public final JMenuItem prepare_JMenuItem(String name) {
+        return new JMenuItem(name);
+    }
+
+    public final JMenuItem prepare_SubJMenuItem(String name) {
+        subMenu = new JMenu(name);
+
+        subMenu.add(this.prepare_JMenuItem("Files or section to HTML"));
+        subMenu.add(this.prepare_JMenuItem("Project to Eclipse"));
+        subMenu.add(this.prepare_JMenuItem("Project to zip"));
+
+        return subMenu;
+    }
+
+    public final JPanel prepare_JPanel() {
+        mainPanel = new JPanel();
+        return mainPanel;
     }
 }
