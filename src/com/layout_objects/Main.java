a8b726914f42015c6790f806a72b9ca4cbfc528e
anguzuDaniel <102733205+anguzuDaniel@users.noreply.github.com>
2022-11-29 11:33:04 +0300

    added tabbed class

--- src/com/layout_objects/Main.java
+++ src/com/layout_objects/Main.java
@@ -10,13 +10,14 @@ public class Main {
 //        Card_layout2 card_layout2 = new Card_layout2();
 //        Gb_Layout gb_layout = new Gb_Layout();
 //        Menus menus = new Menus();
-//        Tab tab = new Tab();
 //        NestedMenu nestedMenu = new NestedMenu();
 //        TestWork testWork = new TestWork();
-//        Test3 test3 = new Test3();
+//        Tab tab = new Tab();
         try {
-            UIManager.setLookAndFeel("com.jtattoo.plaf.acryl.AcrylLookAndFeel");
-            App app = new App();
+            UIManager.setLookAndFeel("com.jtattoo.plaf.bernstein.BernsteinLookAndFeel");
+//            App app = new App();
+            Tab tab = new Tab();
+//        Test3 test3 = new Test3();
         } catch (Exception e) {
         }
     }
--- src/com/layout_objects/Tab.java
+++ src/com/layout_objects/Tab.java
@@ -4,11 +4,15 @@ import javax.swing.*;
 
 public class Tab {
 
+    // Tab constructor
+    // calls the prepareLayout method
     public Tab() {
-        JPanel tabbedPanel = new JPanel();
         this.prepareLayout();
     }
 
+    // creates a frame, and adds the prepareTab()
+    // sets look and feel
+    // sets visibility
     public final void prepareLayout() {
         JFrame.setDefaultLookAndFeelDecorated(true);
         JFrame tabFrame = new JFrame("Tabbed layouts");
@@ -17,18 +21,24 @@ public class Tab {
         tabFrame.setVisible(true);
     }
 
-    private JPanel prepareLabelPane(String name) {
+    // creates an instance of the JPanel class
+    // sets border layout
+    // returns the instance
+    private JPanel preparePanel(String name) {
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createTitledBorder(name));
         return panel;
     }
 
+    // prepares the JTabbedPane
+    // adds all the panels to the JTabbedPane by calling the preparePanel()
+    // returns the tab
     private JTabbedPane prepareTab() {
         JTabbedPane mainTab = new JTabbedPane();
-        mainTab.addTab("Tab one", this.prepareLabelPane("Border Five"));
-        mainTab.addTab("Tab Two", this.prepareLabelPane("Border Two"));
-        mainTab.addTab("Tab Three", this.prepareLabelPane("Border Three"));
-        mainTab.addTab("Tab Four", this.prepareLabelPane("Border Four"));
+        mainTab.addTab("Tab one", this.preparePanel("Border Five"));
+        mainTab.addTab("Tab Two", this.preparePanel("Border Two"));
+        mainTab.addTab("Tab Three", this.preparePanel("Border Three"));
+        mainTab.addTab("Tab Four", this.preparePanel("Border Four"));
         return mainTab;
     }
 }
